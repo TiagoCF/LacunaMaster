a790fcc66714c27c920c14a39dc5d4e0d827a39f
TiagoCF <13144415@stu.mmu.ac.uk>
2016-06-29 01:18:38 +0100

    a

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(4,4);
+	public float jumpSpeed = 1000;
+	private Vector2 movement;
+	private Rigidbody2D rigidbodyComponent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+	}
+
+	// Update physics here, called after Update()
+	void FixedUpdate(){
+		// 5 - Get the component and store the reference
+		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+		// 6 - Move the game object
+		rigidbodyComponent.velocity = movement;
+		//rigidbodyComponent.AddForce (Vector2.up * jumpSpeed);
+        /*
+        if (rigidbodyComponent.velocity.magnitude < 0.5) // speed too low
+            rigidbodyComponent.velocity = Vector2.zero;
+	    */
+    }
+
+}
+
+
+/* Standard functions
+Awake() is called once when the object is created. See it as replacement of a classic constructor method.
+Start() is executed after Awake(). The difference is that the Start() method is not called if the script is not enabled (remember the checkbox on a component in the “Inspector”).
+Update() is executed for each frame in the main game loop.
+FixedUpdate() is called at every fixed framerate frame. You should use this method over Update() when dealing with physics (“RigidBody” and forces).
+Destroy() is invoked when the object is destroyed. It’s your last chance to clean or execute some code.
+
+You also have some functions for the collisions :
+OnCollisionEnter2D(CollisionInfo2D info) is invoked when another collider is touching this object collider.
+OnCollisionExit2D(CollisionInfo2D info) is invoked when another collider is not touching this object collider anymore.
+OnTriggerEnter2D(Collider2D otherCollider) is invoked when another collider marked as a “Trigger” is touching this object collider.
+OnTriggerExit2D(Collider2D otherCollider) is invoked when another collider marked as a “Trigger” is not touching this object collider anymore.
+*/
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float walkSpeed = 800f;
+	private bool isGrounded = true;
+    bool isMoving = false;
+	bool allowHorizontal = true;
+	
+
+
+	Animator animator;
+	public Rigidbody2D rgd;
+	public float jumpSpeed = 500000f;
+
+	//animation states - the values in the animator conditions
+	const int STATE_IDLE = 0;
+	const int STATE_WALK = 1;
+	const int STATE_IDLE_JUMP = 2;
+	const int STATE_WALK_JUMP = 3;
+	const int STATE_CROUCH = 4;
+
+	int currentAnimationState = STATE_IDLE;
+	string currentDirection = "left";
+
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+		rgd = GetComponent <Rigidbody2D>();
+		animator.SetInteger ("state", STATE_IDLE_JUMP);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+
+	void FixedUpdate() {
+		//allow horizontal movement when walking/sprinting > jump/ crouching
+		if (this.animator.GetCurrentAnimatorStateInfo (0).IsName("Idle_Jump") || 
+			this.animator.GetCurrentAnimatorStateInfo (0).IsName("crouch_Idle") || 
+			this.animator.GetCurrentAnimatorStateInfo (0).IsName("crouch_Out") || 
+			this.animator.GetCurrentAnimatorStateInfo (0).IsName("crouch_In")) {allowHorizontal = false;} 
+		else {allowHorizontal = true;}
+
+		//idle animation if no movement keys pressed
+		if (!Input.GetKeyDown("left") && !Input.GetKeyDown("right") && !Input.GetKeyDown("up")&& !Input.GetKeyDown("down") && isGrounded == true){  
+            animator.SetInteger("state", STATE_IDLE);
+            currentAnimationState = STATE_IDLE;
+            isMoving = false;
+        }
+
+		//jump
+		if (Input.GetKeyDown("up") && isGrounded) {	
+			rgd.AddForce (Vector3.up * jumpSpeed);
+			animator.ResetTrigger ("land");
+
+			isGrounded = false;
+			isMoving = true;
+			//walk>jump , horizontal movemente allowed
+			if (Input.GetKey ("right") || Input.GetKey ("left")) {
+				animator.SetTrigger ("WJump");
+				currentAnimationState = STATE_WALK_JUMP;
+				
+			} else {
+				//idle jump, no horzontal movement allowed (for now)
+				allowHorizontal = false;
+				animator.SetInteger ("state", STATE_IDLE_JUMP);
+				
+			}
+		}
+		if (Input.GetKey("down") && isGrounded) {
+			currentAnimationState = STATE_CROUCH;
+			animator.SetInteger ("state", STATE_CROUCH);
+			allowHorizontal = false;
+		}
+		//horizontal movement animations
+		else if ((Input.GetKey("right") || Input.GetKey("left")) && (allowHorizontal) )
+		{
+			
+			animator.SetInteger("state", STATE_WALK);
+            isMoving = true;
+			if (currentAnimationState == STATE_WALK_JUMP) {
+				transform.Translate (Vector2.left * (walkSpeed * 1.9f) * Time.deltaTime);
+			} 
+			else {transform.Translate (Vector2.left * walkSpeed * Time.deltaTime);
+			}
+
+
+            
+        }
+        if (Input.GetKey("left") && !Input.GetKey("right")){changeDirection("left");}		//change direction
+        else if (Input.GetKey("right") && !Input.GetKey("left")) changeDirection("right");
+
+	
+	}
+
+// Check if player has collided with the floor
+	void OnCollisionEnter2D(Collision2D coll)		//collision detection
+	{
+		if (coll.gameObject.name == "Floor")
+		{
+			animator.SetTrigger ("land");
+			isGrounded = true;
+            if (currentAnimationState != STATE_IDLE){
+                animator.SetInteger("state", STATE_IDLE);
+                currentAnimationState = STATE_IDLE;
+            }
+        }
+
+	}
+
+	void changeDirection(string direction)
+	{
+
+		if (currentDirection != direction)
+		{
+			if (direction == "right")
+			{
+				transform.Rotate(0, 180, 0);
+				currentDirection = "right";
+			}
+			else if (direction == "left")
+			{
+				transform.Rotate(0, -180, 0);
+				currentDirection = "left";
+			}
+		}
+
+	}
+
+
+}
